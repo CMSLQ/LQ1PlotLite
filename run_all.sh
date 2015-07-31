cbbd8e310847235c95c607ef6a821be196f1c8d5
Seth Cooper <seth.cooper@cern.ch>
2015-07-31 13:49:32 +0200

    Add limits plots to run_all script.

--- run_all.sh
+++ run_all.sh
@@ -11,3 +11,8 @@ python plot_enujj_finalSelection.py
 python plot_enujj_finalSelection_nMinus1.py
 #python plot_enujj_finalSelection_nMinus1_bestFit.py
 python plot_enujj_preselection.py
+
+# limits
+root -q BR_Sigma_EE_vsMass.C
+root -q BR_Sigma_ENu_vsMass.C
+root -q ComboPlotLQ1.C
