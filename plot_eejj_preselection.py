4c8200291adebdb9b67f5f7e9017db8af315d9e0
Seth Cooper <seth.cooper@cern.ch>
2016-07-26 17:09:15 +0200

    add uncertainty band

--- plot_eejj_preselection.py
+++ plot_eejj_preselection.py
@@ -2,9 +2,51 @@
 
 from plot_common import *
 import tdrstyle
+import math
+from ROOT import kOrange, kGray
+
+
+def GetBackgroundSyst(allBkg, zjets, ttbar, qcd):
+    # /afs/cern.ch/user/m/mbhat/work/public/Systematics_txtfiles_25_07_2016/pre1_sys.dat
+    #  100*(deltaX/X) [rel. change in %]
+    systDict = {
+      'trig' : 1.16081,
+      'pu' : 0.254125,
+      'ereco' : 8.58642,
+      'ees' : 5.40749,
+      'lumi' : 2.7,
+      'heepid' : 9.08357,
+      'jec' : 2.26787,
+      'jer' : 0.0729784,
+      'pdf' : 4.7,
+    }
+    systDictList = systDict.values()
+    systDictList = [value/100.0 for value in systDictList]
+    preselSyst = 0.0
+    for item in systDictList:
+      preselSyst+=pow(float(item),2)
+    # that is on all background
+    ## mine
+    #'qcdNorm' : 40,
+    qcdTerm = pow(qcd*0.4,2)
+    #'ttbarNorm' : 20,
+    ttbarNormTerm = pow(ttbar*0.2,2)
+    #'zjetsNorm' : 2.6,
+    zjetsNormTerm = pow(zjets*0.026,2)
+    ##special
+    # 'ttshape' : 7.31,
+    ttShapeTerm = pow(ttbar*0.0731,2)
+    # 'zshape' : 8.28,
+    zShapeTerm = pow(zjets*0.0282,2)
+    #
+    preselSyst += qcdTerm+ttbarNormTerm+zjetsNormTerm+ttShapeTerm+zShapeTerm
+    preselSyst = math.sqrt(preselSyst)
+    return preselSyst
+
+
 
 # set batch
-r.gROOT.SetBatch()
+#r.gROOT.SetBatch()
 
 
 mass1 = 650
@@ -62,6 +104,16 @@ r.gStyle.SetPadRightMargin(0.1);
 r.gStyle.SetPadTickX(0)
 r.gStyle.SetPadTickY(0)
 
+#print 'GetPreselSyst'
+doSystErr = True
+doRatio = False
+
+if doSystErr:
+  # allbkg, zjets, ttbar, qcd
+  backgroundSyst = GetBackgroundSyst(4237.1,3176.7,897.13,2.7)
+  #print 'BG: 4237.1 +/- '+str(GetBackgroundSyst(4237.1,3176.7,897.13,2.7))
+  backgroundSyst /= 4237.1
+
 #bkgd_file = r.TFile.Open(os.environ["LQDATA"] + "/RunII/eejj_analysis_ttbarRescaleFinalSels_2jun2016/output_cutTable_lq_eejj/analysisClass_lq_eejj_plots.root" )
 #qcd_file  = r.TFile.Open(os.environ["LQDATA"] + "/RunII/eejj_analysis_ttbarRescaleFinalSels_2jun2016/output_cutTable_lq_eejj/analysisClass_lq_eejj_QCD_plots.root")
 bkgd_file = r.TFile.Open(os.environ["LQDATA"] + "/RunII/eejj_analysis_zJetsStCorrectionFinalSelections_21jul/output_cutTable_lq_eejj/analysisClass_lq_eejj_plots.root" )
@@ -96,7 +148,8 @@ for i_var, var in enumerate(vars):
     setStyle (data_hist , 1 ,    0, 1)
     
     data_hist.SetMarkerStyle(20)
-    data_hist.SetMarkerSize (1.5)
+    #data_hist.SetMarkerSize (1.5)
+    data_hist.SetMarkerSize (1.1)
     
     stack = r.THStack ("stack", "stack")
     stack.Add ( qcd_hist   )
@@ -106,8 +159,7 @@ for i_var, var in enumerate(vars):
     stack.Draw()
     stack.SetMaximum(200000)
     stack.SetMinimum(0.1)
-
-    
+    #bkgTotalHist = stack.GetStack().Last() # sum of all TH1 in stack
 
     stack.GetXaxis().SetTitle( x_labels [i_var] )
     stack.GetYaxis().SetTitle( "Events / bin" )
@@ -135,34 +187,115 @@ for i_var, var in enumerate(vars):
     #if 'Mej' in var:
     #  stack.GetXaxis().SetNdivisions(507)
     
-    #leg = r.TLegend(0.42,0.52,0.87,0.88,"","brNDC")
-    leg = r.TLegend(0.43,0.53,0.89,0.89,"","brNDC") #used for all lq2 data plots
-    leg.SetTextFont(42)
-    leg.SetFillColor(0)
-    leg.SetBorderSize(0)
-    leg.SetTextSize(.05)
-    leg.AddEntry(data_hist ,"Data","lpe")
-    leg.AddEntry(zjets_hist,"Z/#gamma* + jets","lf")
-    leg.AddEntry(ttbar_hist,"t#bar{t}","lf")
-    leg.AddEntry(other_hist,"Other background","lf")
-    leg.AddEntry(qcd_hist  ,"Multijet","lf")
-    leg.AddEntry(sig1_hist  ,"LQ, M = "+str(mass1)+" GeV, #beta = 1.0","l")
-    leg.AddEntry(sig2_hist  ,"LQ, M = "+str(mass2)+" GeV, #beta = 1.0","l")
-    
     canv_name = var + "_canv"
     pad_name  = var + "_pad"
     save_name = var 
     #save_name = save_name.replace("PAS", "preselection")
     save_name = save_name + "_eejj.pdf"
-
+    # TEST
     canvas = r.TCanvas(canv_name,canv_name,800,550)
     canvas.cd()
     pad1   = r.TPad( pad_name, pad_name , 0.0, 0.0, 1.0, 1.0 )
     canvas.SetLogy()
-    r.SetOwnership(canvas, False)
-    r.SetOwnership(pad1, False)
-
     stack.Draw("HIST")
+    # TEST
+
+    #canvas = r.TCanvas(canv_name,canv_name,800,550)
+    #pad1  = r.TPad( pad_name+"1", pad_name+"1" , 0.0, 0.0, 1.0, 1.0 )
+    #pad2 = r.TPad()
+    r.SetOwnership(pad1, False)
+    #if doRatio:
+    #    pad1 = r.TPad(pad_name+"1", "", 0.00, 0.20, 0.99, 0.99)
+    #    pad2 = r.TPad(pad_name+"2", "", 0.00, 0.00, 0.99, 0.20)
+    #    pad1.SetFillColor(0)
+    #    pad1.SetLineColor(0)
+    #    pad2.SetFillColor(0)
+    #    pad2.SetLineColor(0)
+    #    r.SetOwnership(pad2, False)
+
+    #canvas.cd()
+    ##canvas.SetLogy()
+    r.SetOwnership(canvas, False)
+    #pad1.cd()
+    #pad1.SetLogy()
+    #stack.Draw("HIST")
+    #
+    ##-- 2nd pad (ratio)
+    #if doRatio:
+    #    h_bkgTot = copy.deepcopy(bkgTotalHist)
+    #    h_ratio = copy.deepcopy(data_hist)
+    #    h_nsigma = copy.deepcopy(data_hist)
+    #    #h_bkgTot1 = TH1F()
+    #    #h_ratio1 = TH1F()
+    #    #h_nsigma1 = TH1F()
+    #    h_bkgTot1 = h_bkgTot
+    #    h_ratio1 = h_ratio
+    #    h_nsigma1 = h_nsigma
+
+    #    #if( self.xbins!="" and self.rebin!="var" ): ## Variable binning
+    #    #    xbinsFinal = array( 'd', self.xbins )
+    #    #    length = len(xbinsFinal)-1
+    #    #    h_bkgTot1 = h_bkgTot.Rebin( length , "h_bkgTot1", xbinsFinal)
+    #    #    h_ratio1 = h_ratio.Rebin( length , "h_ratio1" , xbinsFinal)
+    #    #    h_nsigma1 = h_nsigma.Rebin( length, "h_nsigma1", xbinsFinal)
+    #    #else:
+    #    #    h_bkgTot1 = h_bkgTot
+    #    #    h_ratio1 = h_ratio
+    #    #    h_nsigma1 = h_nsigma
+
+    #    h_ratio1.SetStats(0)
+    #    #if( self.xmin!="" and self.xmax!="" and self.rebin!="var" ):
+    #    #h_bkgTot1.GetXaxis().SetRangeUser(self.xmin,self.xmax-0.000001)
+    #    #h_ratio1.GetXaxis().SetRangeUser(self.xmin,self.xmax-0.000001)
+    #    #h_nsigma1.GetXaxis().SetRangeUser(self.xmin,self.xmax-0.000001)
+
+    #    pad2.cd()
+    #    # fPads2.SetLogy()
+    #    h_ratio1.Divide(h_bkgTot1)
+
+    #    h_ratio1.GetXaxis().SetTitle("")
+    #    h_ratio1.GetXaxis().SetTitleSize(0.06)
+    #    h_ratio1.GetXaxis().SetLabelSize(0.1)
+    #    h_ratio1.GetYaxis().SetRangeUser(0.,2)
+    #    h_ratio1.GetYaxis().SetTitle("Data/MC")
+    #    h_ratio1.GetYaxis().SetLabelSize(0.1)
+    #    h_ratio1.GetYaxis().SetTitleSize(0.13)
+    #    h_ratio1.GetYaxis().SetTitleOffset(0.3)
+    #    h_ratio1.SetMarkerStyle ( 1 )
+
+    #    h_ratio1.Draw("p")
+
+    #    #lineAtOne = TLine(h_ratio.GetXaxis().GetXmin(),1,h_ratio.GetXaxis().GetXmax(),1)
+    #    #lineAtOne.SetLineColor(2)
+    #    #lineAtOne.Draw()
+    #    pad1.cd()
+
+
+    if doSystErr:
+        bgErrs = zjets_hist.Clone()
+        bgErrs.Reset()
+        bgErrs.SetName('bgErrs')
+        for binn in range(0,bgErrs.GetNbinsX()):
+            bgErrs.SetBinContent(binn, zjets_hist.GetBinContent(binn)+ttbar_hist.GetBinContent(binn)+other_hist.GetBinContent(binn)+qcd_hist.GetBinContent(binn))
+        for binn in range(0,bgErrs.GetNbinsX()):
+            bgErrs.SetBinError(binn, bgErrs.GetBinContent(binn)*backgroundSyst)
+        #for binn in range(0,bgErrs.GetNbinsX()):
+        #    print 'bin=',bgErrs.GetBinContent(binn),'+/-',bgErrs.GetBinError(binn)
+        #bgErrs.SetFillColor(kOrange-6)
+        bgErrs.SetFillColor(kGray+2)
+        bgErrs.SetLineColor(kGray+2)
+        bgErrs.SetFillStyle(3001)
+        #bgErrs.SetFillStyle(3018)
+        #bgErrs.SetFillStyle(3013)
+        #bgErrs.SetMarkerSize(1.1)
+        bgErrs.SetMarkerSize(0)
+        #bgErrs.SetLineColor(kOrange)
+        #bgErrs.SetLineWidth(3)
+        #bgErrs.Draw('aE2 aE0 same')
+        #bgErrs.SetDrawOption('hist')
+        #bgErrs.Draw('aE2 E0 same')
+        bgErrs.Draw('E2 same')
+    
     sig1_hist.Draw("HIST SAME")
     sig2_hist.Draw("HIST SAME")
     # convert to Poisson error bars
@@ -173,9 +306,27 @@ for i_var, var in enumerate(vars):
     #g.Draw("ZPSAME")
     g.Draw("ZP0SAME")
 
+
+    #leg = r.TLegend(0.42,0.52,0.87,0.88,"","brNDC")
+    leg = r.TLegend(0.43,0.53,0.89,0.89,"","brNDC") #used for all lq2 data plots
+    leg.SetTextFont(42)
+    leg.SetFillColor(0)
+    leg.SetBorderSize(0)
+    leg.SetTextSize(.05)
+    leg.AddEntry(data_hist ,"Data","lpe")
+    leg.AddEntry(zjets_hist,"Z/#gamma* + jets","lf")
+    leg.AddEntry(ttbar_hist,"t#bar{t}","lf")
+    leg.AddEntry(other_hist,"Other background","lf")
+    if doSystErr:
+      leg.AddEntry(bgErrs, 'Uncertainty band','f')
+    leg.AddEntry(qcd_hist  ,"Multijet","lf")
+    leg.AddEntry(sig1_hist  ,"LQ, M = "+str(mass1)+" GeV, #beta = 1.0","l")
+    leg.AddEntry(sig2_hist  ,"LQ, M = "+str(mass2)+" GeV, #beta = 1.0","l")
     leg.Draw()
+
     canvas.RedrawAxis('G')
     canvas.RedrawAxis()
+    canvas.Modified()
     # redraw frame
     r.gPad.Update()
     line = r.TLine(r.gPad.GetUxmin(),r.gPad.GetUymin(),r.gPad.GetUxmax(),r.gPad.GetUymin())
@@ -197,18 +348,21 @@ for i_var, var in enumerate(vars):
     l2.SetTextSize(0.08)
     l1.DrawLatex(0.64,0.94,lumiEnergyString)
     l2.DrawLatex(0.15,0.84,"CMS")
+    r.gPad.Update()
 
 
-    ### wait for input to keep the GUI (which lives on a ROOT event dispatcher) alive
-    #if __name__ == '__main__':
-    #   rep = ''
-    #   while not rep in [ 'c', 'C' ]:
-    #      rep = raw_input( 'enter "c" to continue: ' )
-    #      if 1 < len(rep):
-    #         rep = rep[0]
+    if not r.gROOT.IsBatch():
+        ## wait for input to keep the GUI (which lives on a ROOT event dispatcher) alive
+        if __name__ == '__main__':
+           rep = ''
+           while not rep in [ 'c', 'C' ]:
+              rep = raw_input( 'enter "c" to continue: ' )
+              if 1 < len(rep):
+                 rep = rep[0]
 
-    print 'saving the canvas'
-    canvas.SaveAs(save_name)
+    exit(0)
+    #print 'saving the canvas'
+    #canvas.SaveAs(save_name)
 
 
 print 'closing files...',
